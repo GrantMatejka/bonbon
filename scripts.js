0557e54644dbb9b33490d62c9c258be29ed4d300
Grant Matejka <grantmatejka1@gmail.com>
2021-02-15 20:56:00 -0800

    More work, local storage yuh

--- scripts.js
+++ scripts.js
@@ -6,7 +6,41 @@
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+const clearTodos = function () {
+  var es = document.querySelectorAll(".todo");
+
+  if (es) {
+    es.forEach(e => e.parentNode.removeChild(e));
+  }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
+  const addTodosToDOM = function (todos) {
+    clearTodos();
+
+    todos.forEach(todo => {
+      // since todo date will be correctly formatted this is safe
+      const currDate = new Date();
+      var todoDate = Date.parse(todo.date);
+      var dateDiff = Math.floor((todoDate - currDate) / (1000 * 3600 * 24)) + 2;
+
+      if (dateDiff < 15) {
+        if (dateDiff <= 0) {
+          var id = "day-0";
+        } else if (dateDiff < 15) {
+          var id = "day-" + dateDiff;
+        }
+
+        var todoColumn = document.getElementById(id);
+        var newTodoElement = document.createElement('p');
+        newTodoElement.innerText = todo.text;
+        newTodoElement.className = "todo";
+
+        todoColumn.appendChild(newTodoElement);
+      }
+    });
+  };
+
   var contentContainerElement = document.getElementById('content');
 
   for (i = 0; i < 15; i++) {
@@ -21,7 +55,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (i > 0) {
       const date = new Date();
       date.setDate(date.getDate() + i - 1);
-  
+
       title.innerText = date.toLocaleDateString();
 
       var subtitle = document.createElement('h5');
@@ -30,11 +64,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
       if (i > 7) {
         el.style.display = 'none';
       }
-  
+
       el.appendChild(subtitle);
-    } else {  
+    } else {
       title.innerText = "Past";
     }
+  }
+
+  // init todos array if not existant
+  var todos = JSON.parse(localStorage.getItem("todos"));
+  if (!todos) {
+    localStorage.setItem("todos", JSON.stringify([]));
+  } else {
+    addTodosToDOM(todos);
   }
 
   const changeDateRange = function (dateRange) {
@@ -67,7 +109,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
       changeDateRange(event.target.firstChild.textContent.trim());
 
       var clickSeq = JSON.parse(localStorage.getItem("clicks"));
-      console.log(clickSeq);
       if (clickSeq) {
         clickSeq.push(event.target.firstChild.textContent);
       } else {
@@ -77,4 +118,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
   });
+
+  // add new todo
+  document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputText = document.getElementById('input').value;
+    const currDate = new Date();
+    const newTodo = { date: currDate, text: inputText };
+
+    var todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (inputText && todos) {
+      todos.push(newTodo);
+    }
+
+    todos.sort((e1, e2) => e1.date < e2.date);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    addTodosToDOM(todos);
+  })
 });
